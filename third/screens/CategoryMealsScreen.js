119fa4ea7b963e546c147595f76c188a719ed4e1
HojinChuu <cnghwls7@gmail.com>
2021-02-18 23:57:07 +0900

    meal

--- third/screens/CategoryMealsScreen.js
+++ third/screens/CategoryMealsScreen.js
@@ -1,30 +1,43 @@
 import React from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
+import { CATEGORIES, MEALS } from "../data/dummy-data";
 
-import { CATEGORIES } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
 const CategoryMealsScreen = ({ navigation }) => {
   const categoryId = navigation.getParam("categoryId");
 
-  const selectedCategory = CATEGORIES.find(
-    (category) => category.id === categoryId
+  const displayedMeals = MEALS.filter(
+    (meal) => meal.categoryIds.indexOf(categoryId) >= 0
   );
+
+  const renderMealItem = (itemData) => {
+    return (
+      <MealItem
+        title={itemData.item.title}
+        image={itemData.item.imageUrl}
+        duration={itemData.item.duration}
+        complexity={itemData.item.complexity}
+        affordability={itemData.item.affordability}
+        onSelectMeal={() => {
+          navigation.navigate({
+            routeName: "MealDetail",
+            params: {
+              mealId: itemData.item.id,
+            },
+          });
+        }}
+      />
+    );
+  };
 
   return (
     <View style={styles.screen}>
-      <Text>The CategoryMeals Screen!</Text>
-      <Text>{selectedCategory.title}</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => {
-          navigation.navigate({ routeName: "MealDetail" });
-        }}
-      />
-      <Button
-        title="Go Back"
-        onPress={() => {
-          navigation.goBack();
-        }}
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item, index) => item.id}
+        renderItem={renderMealItem}
+        style={{ width: "100%" }}
       />
     </View>
   );
@@ -45,6 +58,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    padding: 10,
   },
 });
 
--- third/screens/MealDetailScreen.js
+++ third/screens/MealDetailScreen.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import { MEALS } from "../data/dummy-data";
+
+import CustomHeaderButton from "../components/CustomHeaderButton";
 
 const MealDetailScreen = ({ navigation }) => {
+  const mealId = navigation.getParam("mealId");
+
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
   return (
     <View style={styles.screen}>
-      <Text>The MealDetail Screen!</Text>
+      <Text>{selectedMeal.title}</Text>
       <Button
         title="Go Back to Categories"
         onPress={() => {
@@ -13,6 +21,25 @@ const MealDetailScreen = ({ navigation }) => {
       />
     </View>
   );
+};
+
+MealDetailScreen.navigationOptions = (navigationData) => {
+  const mealId = navigationData.navigation.getParam("mealId");
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+  return {
+    headerTitle: selectedMeal.title,
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+        <Item
+          title="Favorite"
+          iconName="ios-star"
+          onPress={() => {
+            console.log("Mark as favorite!");
+          }}
+        />
+      </HeaderButtons>
+    ),
+  };
 };
 
 const styles = StyleSheet.create({
